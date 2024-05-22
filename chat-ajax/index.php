3577eee4e1c8c4cab6a1e0a2c44b7b39a7dac384
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2024-05-22 16:05:51 +0200

    keydown event

--- chat-ajax/index.php
+++ chat-ajax/index.php
@@ -8,7 +8,7 @@
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script>
         $(document).ready(function() {
-            var chatHistory = document.querySelector('.chat-messages'); 
+            var chatHistory = document.querySelector('.chat-messages');
 
             // Fonction pour charger les messages à partir du fichier PHP
             function loadMessages() {
@@ -20,19 +20,31 @@
             // Gère la soumission du formulaire de chat
             $("form").submit(function(event) {
                 event.preventDefault(); // Empêche le comportement par défaut du formulaire (rechargement de la page)
+                sendMessage(); // Appelle la fonction pour envoyer le message
+            });
+
+            // Gestion de l'envoi du message lorsque l'utilisateur appuie sur la touche "Entrée"
+            $("#chat-message").keydown(function(event) {
+                if (event.keyCode === 13) { // Vérifie si la touche appuyée est "Entrée"
+                    event.preventDefault(); // Empêche le saut de ligne dans le champ de saisie
+                    sendMessage(); // Appelle la fonction pour envoyer le message
+                }
+            });
+
+            // Fonction pour envoyer le message
+            function sendMessage() {
                 var message = $("#chat-message").val(); // Récupère la valeur du message de l'utilisateur
                 // Envoie le message au serveur via une requête POST à 'send_message.php'
-                $.post("send_message.php", {
-                    message: message
-                }, function(data) {
+                $.post("send_message.php", {message: message}, function(data) {
                     $(".chat-messages").html(data); // Met à jour le contenu de 'chat-messages' avec la réponse du serveur
                     $("#chat-message").val(''); // Vide le champ de saisie du message après l'envoi
                     chatHistory.scrollTop = chatHistory.scrollHeight; // Défile vers le bas après l'envoi du message
                 });
-            });
+            }
 
             setInterval(loadMessages, 1000); // Actualise les messages toutes les 3 secondes pour afficher les nouveaux messages
         });
+        F
     </script>
     <title>Ajax Chat</title>
 </head>
